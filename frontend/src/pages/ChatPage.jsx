08fa5b57165ff704b6dc7cd6a59aa4d4fab017ac
Priyaranjan Sunani <prsunani674@gmail.com>
2026-06-17 16:49:59 +0530

    login frontend made

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore.js'
 
 function ChatPage() {
+
+  const { logout } = useAuthStore()
+
   return (
-    <div>
+    <div className='z-10'>
       Chat Page
+      <button onClick={logout} className='auth-btn'>Logout</button>
     </div>
   )
 }
